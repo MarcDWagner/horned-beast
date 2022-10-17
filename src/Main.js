8f4901b4086849782c0280343b04cbebf5bcf6fc
Marc D Wagner <marc.wagner@yahoo.com>
2022-10-17 12:18:44 -0700

    added new src and set up component

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+class Main extends React.component {
+  render(){
+    return (
+      <>
+      <main>
+        <p>Created successfully</p>
+        </main>
+      </>
+    )
+  }
+}
+
+export default Main;
