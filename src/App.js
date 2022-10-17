6902224ddb9bae014021112634bea470d1343c1b
Marc D Wagner <marc.wagner@yahoo.com>
2022-10-17 15:59:55 -0700

    created footer.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@
 import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
+import Footer from './Footer.js';
 import './App.css';
+
 
 
 // class components
@@ -13,6 +15,7 @@ class App extends React.Component{
       <>
       <Header />
       <Main />
+      <Footer />
       </>
     )
   }
