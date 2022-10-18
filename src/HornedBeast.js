6ffafb704336eca39062b5b16fe927fecb029185
Marc D Wagner <marc.wagner@yahoo.com>
2022-10-18 10:59:55 -0700

    set state for votes in hornedbeasts comp

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      votes: 0,
+    }
+  }
+
+  handleVotes = () => {
+    this.setState({
+      votes: this.state.votes + 1
+    })
+  }
+
   render() {
     return (
       <article>
-        
+
         <h2>{this.props.title}</h2>
         {/* <img src={this.props.imgURL} /> */}
 {/* image, src, alt */}
         <p>{this.props.description}</p>
+        <p onClick={this.handleVotes}>Vote here</p>
+        <p>{this.state.votes} Votes</p>
       </article>
     )
   }
